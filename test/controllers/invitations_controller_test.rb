90b24e230ef01cd584cc772ccd93a80e9d8dded6
Chip Irek <chip.irek@gmail.com>
2015-11-29 14:48:18 -0500

    Completed invitation controller tests

--- test/controllers/invitations_controller_test.rb
+++ test/controllers/invitations_controller_test.rb
@@ -1,19 +1,30 @@
 require 'test_helper'
 
 class InvitationsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
+
+
+  setup do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = users(:me)
+    sign_in @user
+    @item = items(:one)
+    @list = lists(:one)
+  end
+
+
+  test "should create a new invitation" do
+    assert_difference('Invitation.count') do
+      #put :mark_incomplete, id: @item, list_id:@item.list_id, format: :js, item: { is_complete: false }
+      post :create, email: 'chipirek@nc.rr.com', list_id: @list.id, format: :js, invitation: { user_id: @user.id, sent_at: Time.now, password_is_temp: false }
+    end
+  end
+
+
+  test "should accept an invitation" do
+    ##put :mark_incomplete, id: @item, list_id:@item.list_id, format: :js, item: { is_complete: false }
+    #get :accept, list_id: @list.id, format: :js, invitation: { user_id: @user.id, sent_at: Time.now, password_is_temp: false }
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
-  test "should get accept" do
-    get :accept
-    assert_response :success
-  end
 
 end
