f605fee92118375f2a805de70dd7694f0b25c464
Chip Irek <chip.irek@gmail.com>
2015-11-29 14:57:04 -0500

    Completed invitation controller tests

--- test/controllers/invitations_controller_test.rb
+++ test/controllers/invitations_controller_test.rb
@@ -14,16 +14,15 @@ class InvitationsControllerTest < ActionController::TestCase
 
   test "should create a new invitation" do
     assert_difference('Invitation.count') do
-      #put :mark_incomplete, id: @item, list_id:@item.list_id, format: :js, item: { is_complete: false }
       post :create, email: 'chipirek@nc.rr.com', list_id: @list.id, format: :js, invitation: { user_id: @user.id, sent_at: Time.now, password_is_temp: false }
     end
   end
 
 
   test "should accept an invitation" do
-    ##put :mark_incomplete, id: @item, list_id:@item.list_id, format: :js, item: { is_complete: false }
-    #get :accept, list_id: @list.id, format: :js, invitation: { user_id: @user.id, sent_at: Time.now, password_is_temp: false }
-    assert_response :success
+    @invitation = invitations(:one)
+    get :accept, list_id: @list.id, id: @invitation.id  #, invitation: { user_id: @user.id, sent_at: Time.now, password_is_temp: false }
+    assert_redirected_to lists_path(assigns(:lists))
   end
 
 
