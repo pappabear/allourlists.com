9aa2a7fc8129975bd08c7b9368f23e4aa0ef6d18
Chip Irek <chip.irek@gmail.com>
2015-11-30 15:25:55 -0500

    Adding "List" entity to automated regression

--- test/integration/automated_regression_test.rb
+++ test/integration/automated_regression_test.rb
@@ -38,6 +38,100 @@ class AutomatedRegressionTest < ActionDispatch::IntegrationTest
       post_via_redirect '/users/sign_in', 'user[email]' => 'chip.irek@gmail.com', 'user[password]' => 'wrong'
       assert_equal '/users/sign_in', path
       assert_equal 'Invalid email or password.', flash[:alert]
+
+    end
+
+
+    test 'create new list' do
+
+      #-- login
+      get '/users/sign_in'
+      assert_response :success
+
+      post_via_redirect '/users/sign_in', 'user[email]' => 'chip.irek@gmail.com', 'user[password]' => 'lollip0p'
+      assert_equal '/', path
+
+      #-- get the index
+      get '/lists'
+      assert_response :success
+      assert assigns(:lists)
+
+      post '/lists', 'list[name]' => 'My Integration Test List', :format => 'js'
+
+      # p 'Verifying...'
+      l = List.last
+      #assert_equal '/lists/' + p.id.to_s, path
+      assert_equal l.name, 'My Integration Test List'
+
+    end
+
+
+    test 'update a list' do
+
+      #-- login
+      get '/users/sign_in'
+      assert_response :success
+
+      post_via_redirect '/users/sign_in', 'user[email]' => 'chip.irek@gmail.com', 'user[password]' => 'lollip0p'
+      assert_equal '/', path
+
+      #-- get the last list created
+      l = List.last
+      t = l.name += ' -e'
+
+      put '/lists/' + l.id.to_s, 'list[name]' => t, :format => 'js'
+
+      assert_equal '/lists/' + l.id.to_s, path
+      l = List.last
+      assert_equal l.name, t
+
+    end
+
+
+    test 'fail adding a list due to missing field' do
+
+      #-- login
+      get '/users/sign_in'
+      assert_response :success
+
+      post_via_redirect '/users/sign_in', 'user[email]' => 'chip.irek@gmail.com', 'user[password]' => 'lollip0p'
+      assert_equal '/', path
+
+      #-- get the last list created
+      l = List.last
+      t = l.name += ' -e'
+
+      test_failed = false
+
+      begin
+        put '/lists/' + l.id.to_s, 'list[name]' => '', :format => 'js'
+      rescue => e
+        assert_equal("Validation failed: Name can't be blank", e.message)
+        test_failed = true
+      end
+
+      assert_equal(test_failed, true)
+
+    end
+
+
+    test 'delete a list' do
+
+      #-- login
+      get '/users/sign_in'
+      assert_response :success
+
+      post_via_redirect '/users/sign_in', 'user[email]' => 'chip.irek@gmail.com', 'user[password]' => 'lollip0p'
+      assert_equal '/', path
+
+      count_before_delete = List.all.count
+      l = List.last
+
+      delete '/lists/' + l.id.to_s, :format => 'js'
+
+      count_after_delete = List.all.count
+      assert_not_equal count_before_delete, count_after_delete
+
     end
 
 
